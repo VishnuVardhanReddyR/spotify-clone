9b70a83e797b48323165798be49df07ae1c77ce7
R Vishnu Vardhan Reddy <vishnurapuru10@gmail.com>
2022-01-09 22:26:17 +0530

    Middle ware commented

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,20 +3,20 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req){
     // Token will exist if user is logged in 
-    const token = await getToken({ req, secret: process.env.JWT_SECRET});
+    // const token = await getToken({ req, secret: process.env.JWT_SECRET});
 
-    const { pathname } = req.nextUrl
+    // const { pathname } = req.nextUrl
     
     // Allow the requests if the following is true...
     // 1) the token exists
     
-    if(pathname.includes("/api/auth") || token ){
-        return NextResponse.next();
-    }
+    // if(pathname.includes("/api/auth") || token ){
+        // return NextResponse.next();
+    // }
 
     // Redirect them to login page id they don't have token AND are requesting a protected route
 
-    if(!token && pathname !== "/login") {
-        return NextResponse.redirect("/login");
-    }
+    // if(!token && pathname !== "/login") {
+        // return NextResponse.redirect("/login");
+    // }
 }
