34ae10e894367cba4eeb0f7ea32aa5c1f5726afc
R Vishnu Vardhan Reddy <vishnurapuru10@gmail.com>
2022-01-09 21:21:06 +0530

    completed to deploy into vercel

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,10 +3,10 @@ import {
     SearchIcon,
     LibraryIcon,
     PlusCircleIcon,
-    HeartIcon,
     RssIcon,
     ArrowLeftIcon,
  } from "@heroicons/react/outline";
+import { HeartIcon } from "@heroicons/react/solid";
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -30,13 +30,8 @@ function Sidebar() {
     return (
         <div className="text-gray-500 p-5 text-sm lg:text-sm border-r
         border-gray-900 overflow-y-scroll scrollbar-hide 
-        h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+        h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36">
             <div className="space-y-4">
-                <button className="flex items-center space-x-2 text-red-300
-                hover:text-red-500" onClick={() => signOut()}>
-                    <ArrowLeftIcon className="h-5 w-5" />
-                    <p>Log Out</p>
-                </button>
                 <button className="flex items-center space-x-2
                 hover:text-white">
                     <HomeIcon className="h-5 w-5" />
@@ -59,14 +54,14 @@ function Sidebar() {
                     <PlusCircleIcon className="h-5 w-5" />
                     <p>Create Playlist</p>
                 </button>
-                <button className="flex items-center space-x-2
-                hover:text-white">
-                    <HeartIcon className="h-5 w-5" />
+                <button className="flex items-center  
+                space-x-2 hover:text-white ">
+                    <HeartIcon className="h-5 w-5 text-blue-500" />
                     <p>Liked Songs</p>
                 </button>
                 <button className="flex items-center space-x-2
                 hover:text-white">
-                    <RssIcon className="h-5 w-5" />
+                    <RssIcon className="h-5 w-5 text-green-500" />
                     <p>Your Episodes</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"/>
